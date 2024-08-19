79731754363f4657c996682d525a7040800f97c5
Brandon Lingo <brandonlingo0@gmail.com>
2024-08-19 01:57:55 -0700

    add test file

---
+++ test/test/test.ino
@@ -0,0 +1,19 @@
+#define RED_LED 13
+#define GREEN_LED 12
+#define BUZZER 11
+#define RECIEVER 10
+#define ECHO 9
+#define TRIG 8
+int stepper_pins[] = {3,5,4,2}; // order of which in the pins need to be passed into Stepper class when initilizing it
+
+void setup() {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(BUZZER, LOW);
+}
